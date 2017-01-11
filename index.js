3f3ad487bf428e0b1a715ab56013cdede58cc607
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-11 21:30:16 +0100

    update server bootstrap
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+	response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
 });
-
-
