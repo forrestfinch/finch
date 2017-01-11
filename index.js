55f0bcd3cfe17a50d917e64c8d59f21a30dafe23
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-11 22:28:09 +0100

    update server bootstrap
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 
 const express = require('express');
 const msnger = require('msnger');
-const app = express();
+const cors = require('cors')
+const app = express()
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const util = require('util')
+const _ = require('lodash')
+
+const app = express()
 
 
 msnger.SERVICE = 'mailgun';
@@ -19,7 +26,14 @@ msnger.BODY = function (req) {
 	return util.format('Message: %s \n%s\n%s', req.body.name, req.body.email);
 }
 
-// // msnger.startServer(); // start the messaging server 
+
+
+app.use(cors()); // enable cross-origin resource sharing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 // 
 app.set('port', (process.env.PORT || 5000));
 
@@ -35,4 +49,41 @@ app.get('/', function (request, response) {
 
 app.listen(app.get('port'), function () {
 	console.log('Node app is running on port', app.get('port'));
+});
+
+/**
+ * Route to send mail
+ * The SMTP transport creates a connection pool that is closed after sending a
+ * mail. This is done because there is no need to maintain a connection all
+ * the time.
+ */
+app.post('/message', function (req, res) {
+	var transporter = nodemailer.createTransport({
+		service: msnger.SERVICE, // like gmail, Mailgun, etc
+		auth: {
+			user: msnger.USERNAME,
+			pass: msnger.PASS
+		}
+	});
+
+	var mailOptions = msnger.setMailOptions(req);
+	console.log(mailOptions);
+
+	transporter.sendMail(mailOptions, function (err, responseStatus) {
+		if (err) {
+			util.log(util.format('ERROR: %j', err));
+			res.status(500).send('Something broke!');
+		} else {
+			util.log(util.format('STATUS: %j', responseStatus));
+			util.log('mail sent!');
+			res.send(true);
+		}
+
+		// Close the SMTP pool.
+		transporter.close();
+		util.log('SMTP pool closed');
+
+		// Clean up
+		transporter = mailOptions = null;
+	});
 });
