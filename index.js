0709475018b4d2ca3933479c6a674713f2c71fb4
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-01-11 22:13:30 +0100

    update server bootstrap
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var express = require('express');
-var app = express();
+'use strict'
 
+const express = require('express');
+const msnger = require('msnger');
+const app = express();
+
+
+msnger.SERVICE = 'mailgun';
+msnger.USERNAME = process.env.USERNAME
+msnger.PASS = process.env.PASSWORD
+msnger.DESTINATION = process.env.EMAIL
+
+
+msnger.SUBJECT = function (req) {
+	return util.format('Important message from %s', req.body.name);
+};
+
+msnger.BODY = function (req) {
+	return util.format('Message: %s \n%s\n%s', req.body.name, req.body.email);
+}
+
+// // msnger.startServer(); // start the messaging server 
+// 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
 	response.render('pages/index');
