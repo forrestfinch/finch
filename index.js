61acf5504524794aadd0c0942b5f525af0a281c3
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-03-03 17:18:24 +0100

    Update index.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- index.js
+++ index.js
@@ -49,6 +49,18 @@ app.get('/', function(request, response) {
 	response.render('index');
 });
 
+app.get('/us', function(request, response) {
+	response.render('about');
+});
+
+app.get('/services', function(request, response) {
+	response.render('services');
+});
+
+app.get('/contact', function(request, response) {
+	response.render('contact');
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
