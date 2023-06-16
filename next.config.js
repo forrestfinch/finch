61f0d8954b2f992ff8e0ad6637709d7a19b5ced4
Bernard Ojengwa <bernardojengwa@gmail.com>
2023-06-16 02:48:09 +0100

    update loader config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/ddeeb6vhx/image/upload/',
+    domains: ['res.cloudinary.com'],
   },
   productionBrowserSourceMaps: true,
   pageExtensions: ['tsx', 'ts'],
